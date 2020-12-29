3d863c5c6f0a69a598c8f6bd738a4fdd520ec414
LucianoFex <LucianoHtml5Esteban1@gmail.com>
2020-12-29 16:52:12 -0300

    feat: Advance in the test file and first steps in the DB

---
+++ BirDayBerDB_manager.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+class Birthdays_db:
+    def __init__(self, connection):
+        #  Preparation of the DB
+        self.connection = sqlite3.connect(connection)
+        self.cursor = self.connection.cursor()
+
+    def close_database(self):
+        self.connection.close()
+
+    def create_table(self, tables):
+        # for table in tables.items():
+        pass
+
+    def add_person(name, surname, country, gender, birth, photo):
+        """
+        Table attributes:
+        NOT NULL: {name, country, gender, birth}
+        NULLABLE: {surname, photo}
+        """
+
+        # sql_query = """
+        # INSERT ...
+        # """
+
+        # self.cursor.executemany()
+        pass
--- test_BirDayBer.py
+++ test_BirDayBer.py
@@ -8,9 +8,29 @@ class BirthDB_testing(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # It should connect the testing DB.
+        id_type = "INTEGER PRIMARY KEY AUTOINCREMENT"
 
-        cls.birth_db = BirDayBerDB_manager.Birthdays_db()
-        cls.birth_db.prepare_database("test_db.db")
+        cls.birth_db = BirDayBerDB_manager.Birthdays_db("test_db.db")
+        cls.birth_db.create_table({
+            "gender": f"id_gender {id_type}, gender VARCHAR(6)",
+
+            "photo": f"id_photo {id_type}, photo BLOB",
+
+            "country": f"id_country {id_type}, country VARCHAR(40)",
+
+            "birth_date": f"""id_birth {id_type},
+            id_country2 INTEGER, birth DATE, age INTEGER,
+            FOREIGN KEY (id_country2) REFERENCES country (id_country)""",
+
+            "person": f"""id_person {id_type}, per_first VARCHAR(35),
+            per_last VARCHAR(35), id_country1 INTEGER,
+            id_gender1 INTEGER, id_birth1 INTEGER, id_photo1 INTEGER,
+            FOREIGN KEY (id_country1) REFERENCES country (id_country),
+            FOREIGN KEY (id_gender1) REFERENCES gender (id_gender),
+            FOREIGN KEY (id_birth1) REFERENCES birth_date (id_birth),
+            FOREIGN KEY (id_photo1) REFERENCES photo (id_photo)"""})
+
+        del id_type
 
     @classmethod
     def tearDownClass(cls):
@@ -30,46 +50,46 @@ class BirthDB_testing(unittest.TestCase):
     def tearDown(self, cls):
         # All rows deletion.
 
-        cls.birth_db.remove_people([1, 2])  # id 1 = Franco | id 2 = Randolph
+        cls.birth_db.remove_person([1, 2])  # id 1 = Franco | id 2 = Randolph
 
     def test_check_names(self, cls):
-        self.all_names = cls.birth_db.people_names()
+        self.all_names = cls.birth_db.person_names()  # All
 
         self.assertEqual(self.all_names, ("Franco", "Randolph"))
         self.assertEqual(len(self.all_names), 2)
 
     def test_check_surnames(self, cls):
-        self.all_surnames = cls.birth_db.people_surnames()
+        self.all_surnames = cls.birth_db.person_surnames()  # All
 
         self.assertEqual(self.all_surnames, ("Frias", "Carter"))
         self.assertEqual(len(self.all_surnames), 2)
 
     def test_check_births(self, cls):
-        self.all_births = cls.birth_db.people_birth()
+        self.all_births = cls.birth_db.person_birth()  # All
 
         self.assertEqual(self.all_births, ("2003-11-18", "1919-12-23"))
         self.assertEqual(len(self.all_births), 2)
 
     def test_check_ages(self, cls):
-        self.all_ages = cls.birth_db.people_age()
+        self.all_ages = cls.birth_db.person_age()  # All
 
         self.assertEqual(self.all_ages, ("Frias", "Carter"))
         self.assertEqual(len(self.all_ages), 2)
 
     def test_check_countries(self, cls):
-        self.all_countries = cls.birth_db.people_age()
+        self.all_countries = cls.birth_db.person_country()  # All
 
         self.assertEqual(self.all_countries, ("Argentina", "United States"))
         self.assertEqual(len(self.all_countries), 2)
 
     def test_check_genders(self, cls):
-        self.all_genders = cls.birth_db.people_age()
+        self.all_genders = cls.birth_db.person_gender()  # All
 
         self.assertEqual(self.all_genders, ("Male", "Male"))
         self.assertEqual(len(self.all_genders), 2)
 
     def test_check_photos(self, cls):
-        self.all_photos = cls.birth_db.people_age()
+        self.all_photos = cls.birth_db.person_photo()
 
         self.assertEqual(self.all_photos, (None, None))
         self.assertEqual(len(self.all_photos), 2)
