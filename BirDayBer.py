b881a97614aa0e95b5c4b213c4b6119ce28760c1
LucioFex <luciano1D2esteban@gmail.com>
2021-03-27 15:46:14 -0300

    feat: Implementation of the "people_finder" label

--- BirDayBer.py
+++ BirDayBer.py
@@ -413,16 +413,21 @@ class Birdayber(Birdayber_setUp):
             self.finder_border_left, bg="#5d8999")
 
         self.people_over = tk.Label(
-            self.finder_border_left, relief=tk.FLAT, text="People",
+            self.finder_background, relief=tk.FLAT, text="People",
             font=("Century Gothic", round(self.screen_width / 60)),
             width=round(self.screen_width / 74),
             bg="#5f99af", fg="#e7e7e7")
+
+        self.people_finder = tk.Label(
+            self.finder_background,
+            width=round(self.screen_width / 22.5))
 
         self.left_middle.pack(pady=(self.screen_height * 1.7 / 100, 0))
         self.finder_border_bottom.pack()
         self.finder_border_left.pack()
         self.finder_background.pack()
         self.people_over.pack(ipadx=(self.screen_width / 33.5))
+        self.people_finder.pack()
 
 
 if __name__ == '__main__':
