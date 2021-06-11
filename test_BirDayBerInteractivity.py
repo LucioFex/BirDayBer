a4876913a82776fc616f9cd8e92cd24e88efd92f
LucioFex <luciano1D2esteban@gmail.com>
2021-06-11 16:31:43 -0300

    test: Add of new a new test and general update of the 'test_BirDayBerInteractivity.py' file

--- test_BirDayBerInteractivity.py
+++ test_BirDayBerInteractivity.py
@@ -26,20 +26,38 @@ class BirDayBerInteractivity_testing(unittest.TestCase):
         cls.interface.drop_database()
 
     def setUp(self):
-        pass
+        self.interface.add_person({
+            "countries": {"country": "Argentina"},
+            "genders": {"gender": "Male"},
+            "photos": {"photo": 'testing//image_test.png'},
+            "births": {"birth": "2003-07-15"},
+            "people": {"per_first": "Severus", "per_last": "Snape"}})
+        self.interface.add_person({
+            "countries": {"country": "United States"},
+            "genders": {"gender": "Male"},
+            "photos": {"photo": None},
+            "births": {"birth": "1919-12-23"},
+            "people": {"per_first": "Randolph", "per_last": "Carter"}})
 
     def tearDown(self):
-        pass
+        self.interface.remove_all_people()
 
     def test_settings_button(self):
         self.assertFalse(self.interface.get_settings())
         self.interface.open_settings()
 
         self.assertTrue(self.interface.get_settings())
-        self.assertEqual(len(self.interface.settings.winfo_children), 5)
+        self.assertEqual(len(self.interface.settings.winfo_children()), 5)
 
         self.interface.close_settings()
         self.assertFalse(self.interface.get_settings())
+
+    def test_people_view(self):
+        self.assertTrue(len(self.interface.people_finder.winfo_children()), 2)
+        self.interface.remove_person(1)
+        self.assertTrue(len(self.interface.people_finder.winfo_children()), 1)
+        self.interface.remove_person(2)
+        self.assertTrue(len(self.interface.people_finder.winfo_children()), 0)
 
 
 if __name__ == "__main__":
